fd027204158f39449354e33b2083353b67631100
Danielo <danielm98060509@gmail.com>
2025-06-24 19:37:41 -0400

    Feature: Working on navbar.

--- src/components/BottomNavbar.vue
+++ src/components/BottomNavbar.vue
@@ -1,32 +1,33 @@
 <template>
-  <div class="fixed inset-x-0 bottom-0 bg-white shadow-lg p-3 flex justify-around items-center rounded-t-xl z-50">
+  <div style="box-shadow: 0px -10px 20px rgba(0, 0, 0, 0.1)" class="fixed flex space-x-4 justify-center items-center inset-x-0 bottom-0
+    bg-white p-3 z-50">
     <router-link
       to="/pokemon"
-      class="flex-1 flex items-center justify-center p-2 rounded-lg transition-colors duration-200"
+      class="flex items-center justify-center p-2 w-1/3 rounded-full transition-colors duration-200"
       :class="{
-        'bg-red-600 text-white': isAllActive,
-        'text-gray-600 hover:bg-gray-100': !isAllActive
+        'bg-primary text-white': isAllActive,
+        'text-white bg-light-gray': !isAllActive
       }"
     >
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
         <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
       </svg>
-      <span class="text-xs font-semibold ml-2">All</span>
+      <span class="ml-2">All</span>
     </router-link>
 
     <router-link
       to="/pokemon/favorites"
-      class="flex-1 flex items-center justify-center p-2 rounded-lg transition-colors duration-200 ml-2"
+      class="flex items-center justify-center p-2 w-1/3 rounded-full transition-colors duration-200"
       :class="{
-        'bg-gray-300 text-gray-800': isFavoritesActive,
-        'text-gray-600 hover:bg-gray-100': !isFavoritesActive
+        'bg-primary text-white': isFavoritesActive,
+        'text-white bg-light-gray': !isFavoritesActive
       }"
     >
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
-      </svg>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="text-light-bg size-6">
+        <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clip-rule="evenodd" />
+      </svg>  
 
-      <span class="text-xs font-semibold ml-2">Favorites</span>
+      <span class="ml-2">Favorites</span>
     </router-link>
   </div>
 </template>
--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -4,16 +4,16 @@
     @click="viewDetails"
   >
     <span class="text-lg font-semibold text-gray-800 capitalize">{{ pokemon.name }}</span>
-    <button @click.stop="toggleFavorite" class="focus:outline-none">
-      <svg
-        class="w-6 h-6"
-        :class="{ 'text-red-500': pokemon.isFavorite, 'text-gray-400': !pokemon.isFavorite }"
-        fill="currentColor"
-        viewBox="0 0 24 24"
-      >
-        <path
-          d="M12 21.35l-1.84-1.66C4.41 15.36 2 13.04 2 10.5 2 7.72 4.22 5.5 7 5.5c1.81 0 3.55.9 4.5 2.36C13.45 6.4 15.19 5.5 17 5.5c2.78 0 5 2.22 5 5s-2.41 4.86-8.16 14.19z"
-        />
+    <button @click.stop="toggleFavorite" class="focus:outline-none cursor-pointer">
+
+      <svg v-if="pokemon.isFavorite" width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="22" cy="22" r="22" fill="#F5F5F5"/>
+        <path d="M20.6052 9.90444L17.4318 16.627L10.3316 17.7086C9.05834 17.9015 8.54806 19.5415 9.47142 20.4809L14.6082 25.7107L13.3933 33.0984C13.1746 34.4338 14.5208 35.4341 15.6482 34.8096L22 31.3213L28.3518 34.8096C29.4792 35.429 30.8254 34.4338 30.6067 33.0984L29.3918 25.7107L34.5286 20.4809C35.4519 19.5415 34.9417 17.9015 33.6684 17.7086L26.5682 16.627L23.3948 9.90444C22.8262 8.70615 21.1787 8.69092 20.6052 9.90444Z" fill="#ECA539"/>
+      </svg>
+
+      <svg v-else width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="22" cy="22" r="22" fill="#F5F5F5"/>
+        <path d="M20.6052 9.90444L17.4318 16.627L10.3316 17.7086C9.05834 17.9015 8.54806 19.5415 9.47142 20.4809L14.6082 25.7107L13.3933 33.0984C13.1746 34.4338 14.5208 35.4341 15.6482 34.8096L22 31.3213L28.3518 34.8096C29.4792 35.429 30.8254 34.4338 30.6067 33.0984L29.3918 25.7107L34.5286 20.4809C35.4519 19.5415 34.9417 17.9015 33.6684 17.7086L26.5682 16.627L23.3948 9.90444C22.8262 8.70615 21.1787 8.69092 20.6052 9.90444Z" fill="#BFBFBF"/>
       </svg>
     </button>
   </div>
