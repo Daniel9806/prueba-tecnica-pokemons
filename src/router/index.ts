6567fe7983fb9a8b3a241835c4e8558542efd267
Danielo <danielm98060509@gmail.com>
2025-06-25 13:07:50 -0400

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import WelcomeView from '@/views/WelcomeView.vue'
 import PokemonListView from '@/views/PokemonListView.vue'
 import FavoritePokemonView from '@/views/FavoritePokemonView.vue'
-import EmptyListView from '@/views/EmptyListView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,11 +20,6 @@ const router = createRouter({
       path: '/pokemon/favorites',
       name: 'favorite-pokemon',
       component: FavoritePokemonView
-    },
-    {
-      path: '/empty',
-      name: 'empty-list',
-      component: EmptyListView
     },
     {
       path: '/:catchAll(.*)',
